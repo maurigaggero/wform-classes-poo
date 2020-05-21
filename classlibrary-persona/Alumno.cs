1db8cc0c1701533e9eef1c94a18d0129efbdde91
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-05-21 15:42:37 -0300

    Add clases docente y alumno

---
+++ classlibrary-persona/Alumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classlibrary_persona
+{
+    class Alumno : Persona
+    {
+        #region Atributos
+        private string carrera;
+        private long legajo;
+        #endregion
+
+        #region Propiedades
+        public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+        public long Legajo
+        {
+            get { return legajo; }
+            set { legajo = value; }
+        }
+        #endregion
+
+        #region Constructores
+        public Alumno()
+        {
+        }
+
+        public Alumno(string Nom, long DNI, DateTime FecNac, char Sex, string Carr, long Leg)
+        {
+            Nombre = Nom;
+            Dni = DNI;
+            FechNac = FecNac;
+            Sexo = Sex;
+            Carrera = Carr;
+            Legajo = Leg;
+        }
+        #endregion
+
+
+        #region Métodos
+        #endregion
+    }
+}
---
+++ classlibrary-persona/Docente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classlibrary_persona
+{
+    class Docente : Alumno
+    {
+        #region Atributos
+        private string materia;
+        private string legajo;
+        #endregion
+
+        #region Propiedades
+        public string Materia
+        {
+            get { return materia; }
+            set { materia = value; }
+        }
+        public new string Legajo
+        {
+            get { return legajo; }
+            set { legajo = value; }
+        }
+        #endregion
+
+        #region Constructores
+        public Docente()
+        {
+        }
+
+        public Docente(string Nom, long DNI, DateTime FecNac, char Sex, string Mat, string Leg)
+        {
+            Nombre = Nom;
+            Dni = DNI;
+            FechNac = FecNac;
+            Sexo = Sex;
+            Materia = Mat;
+            Legajo = Leg;
+        }
+        #endregion
+
+
+        #region Métodos
+        #endregion
+    }
+}
